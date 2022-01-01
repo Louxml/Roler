2b97b4ee6e76d25217c5b232d9882e4eddc52eec
Louxml <1764097857@qq.com>
2022-01-01 14:19:35 +0800

    [init]dev

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 var express = require("express")
 var app = express();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
 
-app.use('/public',express.static('public'));
+app.use('/package',express.static('package'));
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/demo',(req,res) => {
-    res.sendFile(__dirname + '/demo.html');
-});
 
-io.on('connection',(socket) => {
-    socket.on('cmd', (data) => {
-        io.emit('cmd',data)
-    })
-})
-
-http.listen(3000,() => {
-    console.log("listening to 3000");
+http.listen(8060,() => {
+    console.log("listening to 8060");
 })

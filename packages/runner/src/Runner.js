11a953fbd0b35aebf8cfdaa21220891ffefe025a
feiyongjun <feiyongjun@elestorom.com>
2022-07-29 15:59:12 +0800

    [fix]

--- packages/runner/src/Runner.js
+++ packages/runner/src/Runner.js
@@ -4,15 +4,21 @@ import { RunListener } from "./RunListener.js";
 // 暂定常量
 const deltatime = 1/60
 
+// 默认配置
+const config = {
+    autoStart: false
+}
+
 /**
  * 运行器
  * 可优化：
- * 1、运行期的开始和结束可以判断是否有监听器，有则开始，无则停止
- * 2、时间戳上可优化，第一帧时间戳好像有问题
- * 3、还没考虑帧率的问题，默认requestAnimationFrame帧率
+ * 1、还没考虑帧率的问题，默认requestAnimationFrame帧率（最高帧率）
  */
 export class Runner{
-    
+    /**
+     * 类实例
+     * @private
+     */
     static _system;
     /**
      * id
@@ -84,11 +90,13 @@ export class Runner{
         }
 
         // 配置
-        if (options.autoStart) this._started = true;
+        Object.assign(config, options);
+
+        // 链表的控制节点
         this._head = new RunListener(null, null, false, Infinity);
 
 
-        this._runner = (time) => {
+        this._runner = (time = performance.now()) => {
             if (this._started){
                 this._update(time)
                 if (this._started){
@@ -99,6 +107,11 @@ export class Runner{
         this._runner()
     }
 
+    /**
+     * 开始运行器
+     * @public
+     * @returns this 返回改对象（用于链式调用）
+     */
     start(){
         if (!this._started){
             this._started = true;
@@ -107,6 +120,11 @@ export class Runner{
         return this;
     }
 
+    /**
+     * 暂停运行器
+     * @public
+     * @returns this 返回改对象（用于链式调用）
+     */
     stop(){
         if (this._started){
             this._started = false;
@@ -115,18 +133,42 @@ export class Runner{
         return this;
     }
 
+    /**
+     * 添加帧粒子函数
+     * @param {Function} fn 帧粒子函数
+     * @param {Object} context 函数上下文环境
+     * @param {Boolean} once 是否仅运行一次
+     * @param {Number} priority 优先级
+     * @public
+     * @returns this 返回改对象（用于链式调用）
+     */
     add(fn = ()=>{}, context = null, once = false, priority = UPDATE_PRIORITY.NORMAL){
         let listener = new RunListener(fn, context, once, priority);
         this._addListener(listener)
         return this;
     }
 
+    /**
+     * 添加仅运行一次的帧粒子函数
+     * @param {Function} fn 帧粒子函数
+     * @param {Object} context 函数上下文环境
+     * @param {Number} priority 优先级
+     * @public
+     * @returns this 返回改对象（用于链式调用）
+     */
     addOnce(fn = ()=>{}, context = null, priority = UPDATE_PRIORITY.NORMAL){
         let listener = new RunListener(fn, context, this, priority);
         this._addListener(listener)
         return this;
     }
 
+    /**
+     * 移除帧粒子函数
+     * @param {Function} fn 帧粒子函数
+     * @param {Object} context 函数上下文环境
+     * @public
+     * @returns this 返回改对象（用于链式调用）
+     */
     remove(fn = ()=>{}, context = null){
         const head = this._head;
         let listener = head.next;
@@ -140,6 +182,10 @@ export class Runner{
         return this;
     }
 
+    /**
+     * 销毁运行器
+     * @public
+     */
     destroy(){
         if (!this._protected){
             this.stop();
@@ -170,6 +216,7 @@ export class Runner{
     }
 
     /**
+     * 开始帧动画
      * @private
      */
     _requestAnimationFrame(){
@@ -179,6 +226,7 @@ export class Runner{
     }
 
     /**
+     * 停止帧动画
      * @private
      */
     _cancelAnimationFrame(){
@@ -193,15 +241,17 @@ export class Runner{
      * @param {number} time 运行时间戳
      * @private
      */
-    _update(time = performance.now()){
+    _update(time){
         this._deltatime = time - this._lasttime;
         
         const head = this._head;
         let listener = head.next;
         while (listener){
+            // 执行更新事件
             listener = listener.emit(this._deltatime)
         }
 
+        // 更新上一次时间戳
         this._lasttime = time;
     }
 }
